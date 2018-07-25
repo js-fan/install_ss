9d39966e30486fc4963c69f5ae246573d4447649
Junsong Fan <junsongfc76@gmail.com>
2018-07-25 15:37:41 +0800

    Init commit

---
+++ install_ss.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+PASSWORD=cancan.fan
+SERVER_PORT=20863
+METHOD=aes-256-cfb
+TIMEOUT=300
+FILENAME=../ss_config.json
+
+
+export LC_ALL=C
+apt-get update
+apt-get install build-essential git python-pip
+
+git clone https://github.com/shadowsocks/shadowsocks.git
+cd shadowsocks && git checkout master
+pip install . && cd ..
+
+echo "{" > $FILENAME
+echo "\"server\":\"::\"," >> $FILENAME
+echo "\"server_port\":$SERVER_PORT," >> $FILENAME
+echo "\"method\":\"$METHOD\"," >> $FILENAME
+echo "\"password\":\"$PASSWORD\"," >> $FILENAME
+echo "\"timeout\":$TIMEOUT" >> $FILENAME
+echo "}" >> $FILENAME
+cat $FILENAME
+
+
+# BBR
+# wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh
+# chmod +x bbr.sh && ./bbr.sh
+
+# BBR_beta
+chmod +x BBR_POWERED.sh && bash BBR_POWERED.sh
